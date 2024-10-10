0a6009f8aa046db971f8ce3a660e88453255a4b2
jujemu <jujemu30@gmail.com>
2024-10-10 13:48:24 +0900

    Refactor: optimize include statements

--- S/server.h
+++ S/server.h
@@ -1,10 +1,7 @@
 #include "server_tls.h"
 
 #define BUF_SIZE 256
-#define PORT 443
 #define SOCK_SIZE 10
-#define CERTIFICATE_PATH "/root/projects/echo/S/certificate/server.crt"
-#define KEY_PATH "/root/projects/echo/S/certificate/server.key"
 
 int echo(ssl_client* clients, fd_set* read_fd, int curr_sock, char* buf, int fd_max, int serv_sock, int top);
 void attach_noti(char* write_buf, char* buf, int sock);
--- S/server_socket.h
+++ S/server_socket.h
@@ -1,6 +1,4 @@
-#define BUF_SIZE 256
 #define PORT 443
-#define SOCK_SIZE 10
 
 void bind_serv_sock(int serv_sock);
 int accept_and_create_client_sock(int serv_sock);
