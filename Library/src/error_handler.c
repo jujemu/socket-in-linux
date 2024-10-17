d18cd22910393a8253a1b2d5f3a677880dc67571
jujemu <jujemu30@gmail.com>
2024-10-18 08:00:27 +0900

    Refactor: client_tls in Library

--- Library/src/error_handler.c
+++ Library/src/error_handler.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "config.h"
+
 void error_handle(char* msg) {
     printf("%s\n\n", msg);
     exit(-1);
+}
+
+void show_last_error_msg() {
+    char msg_buf[BUF_SIZE] = { 0, };
+    unsigned long err_num = ERR_get_error();
+
+    printf("------ ERROR ------\n");
+    printf("err_num : %ld\n", err_num);
+    ERR_error_string(err_num, msg_buf);
+    printf("err: %s\n", msg_buf);
+    printf("------ ERROR ------\n");
 }
